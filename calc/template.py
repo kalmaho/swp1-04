8bd70cec516f1754ba47f7b6473329456620ddbe
kalmaho <dhsh1214@naver.com>
2020-06-22 22:50:43 +0900

    issue #2 Add WSGIfile in calc

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = b"""
+<html>
+    <body>
+        <form method="get" action="">
+            <input type="number" name="a"> 
+            <input type="number" name="b">
+            <input type="submit">
+        </form>
+        <p>
+          sum:%(sum)s</br>
+          mul:%(mul)s</br>
+        <p>
+    </body>
+</html>
+"""
